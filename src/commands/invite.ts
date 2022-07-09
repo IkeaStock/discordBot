98951784c7df73afde0fe017a30801b66b0c4f17
devramsean0 <outramsean0@gmail.com>
2022-07-09 20:40:43 +0000

    Invite Command

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -2,11 +2,12 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { type CommandOptions, Command, ChatInputCommand } from '@sapphire/framework';
 import type { CommandInteraction } from 'discord.js';
 @ApplyOptions<CommandOptions>({
-	description: 'Invite me!'
+	description: 'Invite me!',
+	name: 'invite'
 })
 export class UserCommand extends Command {
 	public async chatInputRun(interaction: CommandInteraction) {
-        interaction.reply(`Invite me using:\n (this url)[${process.env.WEB_DOMAIN}/discordBot/invite]`)
+        interaction.reply(`Invite me using [this url](${process.env.WEB_DOMAIN}/discordBot/invite)`)
 	}
 	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
 		registry.registerChatInputCommand((builder) =>
